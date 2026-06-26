c77ddba72f24438a081c9d08c4ab8a0c6331d0b0
Ira <iwracklee970@gmail.com>
2026-06-26 13:51:31 +0400

    Update vite base url for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base: '/TSAA_GAME_IK_GOLDEN_FLEECE/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
